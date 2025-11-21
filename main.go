69cac72d7aeb00ea3ab87d6580145811877c844c
rudy bundela <rudranujbundella@gmail.com>
2025-11-21 23:42:37 +1300

    add pairing endpoint and connect endpoint to swap forms in and out

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"camcpy/main/handlers"
 
 	"github.com/a-h/templ"
+	"github.com/starfederation/datastar-go/datastar"
 )
 
 var dev = true
@@ -34,7 +35,12 @@ func main() {
 				http.FileServer(http.Dir("static")))))
 
 	mux.Handle("/", templ.Handler(components.Index()))
-	mux.Handle("/formendpoint", http.HandlerFunc(handlers.HandleTest))
+	mux.Handle("/pairingendpoint", http.HandlerFunc(handlers.HandlePairing))
+	mux.Handle("/connectendpoint", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sse := datastar.NewSSE(w, r)
+		sse.PatchElementTempl(components.ConnectForm(), datastar.WithSelectorID("pairform"))
+		sse.PatchElementTempl(components.CodePen([]string{"Waiting for input..."}))
+	}))
 
 	fmt.Println("Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
