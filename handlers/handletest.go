da36440ffcd5e1bf25a3271c1aae2e8a7247bd02
rudy bundela <rudranujbundella@gmail.com>
2025-11-21 21:12:21 +1300

    more changes to pairform

--- handlers/handletest.go
+++ handlers/handletest.go
@@ -2,89 +2,49 @@
 package handlers
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os/exec"
-	"strings"
+
+	"camcpy/main/components"
 
 	"github.com/starfederation/datastar-go/datastar"
 )
 
 func HandleTest(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
-	fmt.Println(r.Form)
 	formvalues := r.Form
 
 	// TODO: Form validation
 
 	sse := datastar.NewSSE(w, r)
-	resultingOutput := runCommand(formvalues)
-	alertOutput := fmt.Sprintf("console.log('%s')", resultingOutput)
-	alertOutput = strings.ReplaceAll(alertOutput, "\n", "")
-	fmt.Println(alertOutput)
+	resultingOutput, _ := runCommand(formvalues)
+	if err := sse.ConsoleLog("Returned from runCommand"); err != nil {
+		log.Println(err)
+	}
+	newComponent := components.CodePen(string(resultingOutput))
 
-	if err := sse.ExecuteScript(alertOutput); err != nil {
-		log.Fatal(err)
+	if err := sse.PatchElementTempl(newComponent); err != nil {
+		log.Println(err)
 	}
 }
 
-func runCommand(formvalues url.Values) (resultingoutput string) {
+func runCommand(formvalues url.Values) (resultingOutput []byte, err error) {
 	deviceAddress := fmt.Sprintf("%s:%s", formvalues.Get("ipaddr"), formvalues.Get("port"))
 	deviceCode := formvalues.Get("code")
 	fmt.Printf("Device address = %s; code = %s\n", deviceAddress, deviceCode)
 
 	cmd := exec.Command("adb", "pair", deviceAddress, deviceCode)
+	fmt.Println("Running command...")
 
-	stdin, err := cmd.StdinPipe()
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Panicf("Error creating StdinPipe: %v\n", err)
-	}
-	defer stdin.Close()
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Panicf("Error creating StdoutPipe: %v\n", err)
-	}
-	defer stdout.Close()
-
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		log.Panicf("Error creating StderrPipe: %v\n", err)
-	}
-	defer stderr.Close()
-
-	if err := cmd.Start(); err != nil {
-		log.Panicf("Error starting adb pair command: %v\n", err)
+		log.Printf("Error starting adb pair command: %v\n", err)
 	}
 
-	go func() {
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			fmt.Println("Scanner stdout output = ", scanner.Text())
-		}
-	}()
-
-	go func() {
-		scanner := bufio.NewScanner(stderr)
-		for scanner.Scan() {
-			fmt.Println("Scanner stderr output = ", scanner.Text())
-		}
-	}()
-
-	errScanner := bufio.NewScanner(stderr)
-
-	for errScanner.Scan() {
-		fmt.Println("errScanner output = ", errScanner.Text())
-	}
-
-	if err := cmd.Wait(); err != nil {
-		fmt.Printf("adb pair command finished with error: %v\n", err)
-	}
-
-	return "hello"
+	return output, err
 }
